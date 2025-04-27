e3712239b12d31bbda3e0c28b6c9927f397ae7ab
Sushant Charaya <51811412+sc2609@users.noreply.github.com>
2025-04-27 19:40:36 +0530

    Update bank_validator.py

--- bank_validator.py
+++ bank_validator.py
@@ -180,9 +180,9 @@ def detect_image_forgery(uploaded_file_path):
 
 # --------------- Fraud Detection Pipeline ---------------
 def fraud_detection_pipeline(extracted_data):
-    account_number = extracted_data.get("account_holder_account_number", "")
-    bank_name = extracted_data.get("bank_name", "")
-    bank_address = extracted_data.get("bank_address", "")
+    account_number = extracted_data.account_holder_account_number or ""
+    bank_name = extracted_data.bank_name or ""
+    bank_address = extracted_data.bank_address or ""
 
     print("\n🚀 Running Global Fraud Detection...")
 
